1f967e949015d055d2bc185717f451a0c3f26416
vincenzo <1084062@gmail.com>
2020-07-01 13:03:05 +0200

    modifiche minori

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent  {
   statoFlussi = ['Generato','Generazione Fallita','Inviato','Invio Fallito'];
   tipoFlussi = ['Bonifico Bancario','Pagamento diretto con rilascio quietanza'];
 
-  model = new TassaAuto(18, '', this.statoFlussi[0], this.tipoFlussi[0],'01-01-2000', '31-12-2000');
+  model = new TassaAuto(0, '', this.statoFlussi[0], this.tipoFlussi[0],'01-01-2020', '31-12-2020');
 
   submitted = false;
 
@@ -21,15 +21,21 @@ export class HomeComponent  {
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
 
-  TassaAuto() {
-    this.model = new TassaAuto(42,'','','','','');
+  Pulisci() {
+    this.model = new TassaAuto(0,'','','','','');
   }
 
-  skyDog(): TassaAuto {
+  InviaRicerca() {
+    alert("invio dati a BeckEnd");
+   
+    console.log(this.model);
+
+  }
+  /*skyDog(): TassaAuto {
     let myTassa =  new TassaAuto(42, 'SkyDog','Fetch any object at any distance','Leslie Rollover','','');
     console.log('Chiamo tassaAuto ' + TassaAuto.name); // "My hero is called SkyDog"
     return myTassa;
-  }
+  }*/
 
   //////// NOT SHOWN IN DOCS ////////
 
